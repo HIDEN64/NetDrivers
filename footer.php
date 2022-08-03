e18d9c22dc4e268c00ecec4473a3cd5d7c94b319
subpurple <102702106+subpurple@users.noreply.github.com>
2022-08-03 12:19:17 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
 <?php
 declare(strict_types=1);
 ?>
-<hr><p class="copyright"><i>Copyright <a href="https://nickandfloppy.com/" target="_blank">nick and floppy</a> <?=date('Y')?></i></p>
+<hr><p class="copyright"><i>&copy; <a href="https://nickandfloppy.com/" target="_blank">nick and floppy</a> <?=date('Y')?></i></p>
