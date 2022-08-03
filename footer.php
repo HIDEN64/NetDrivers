396419814c60ff5c37539f3adbd37cf60f5dcfe3
subpurple <102702106+subpurple@users.noreply.github.com>
2022-08-03 12:18:13 -0400

    succ

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
 <?php
 declare(strict_types=1);
-echo '<hr><p class="copyright"><i>&copy; ' . date('Y'). ' <a href="https://nickandfloppy.com/" target="_blank">nick and floppy</a></i></p>';
 ?>
+<hr><p class="copyright"><i>Copyright <a href="https://nickandfloppy.com/" target="_blank">nick and floppy</a> <?=date('Y')?></i></p>
