cec980d46d34ff11014a738373c26a25b4507040
Frankie B <floppydisk05@aol.com>
2022-08-07 21:10:19 +0000

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,4 +1,2 @@
-<?php
-declare(strict_types=1);
-?>
+<?php declare(strict_types=1); ?>
 <hr><p class="copyright"><i>&copy; <?=date('Y')?> <a href="https://nickandfloppy.com/" target="_blank">nick and floppy</a></i></p>
