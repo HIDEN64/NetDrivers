6098987837287de379533f43b48da7423a789ec1
Frankie B <floppydisk05@aol.com>
2022-08-07 21:07:55 +0000

    Move copyright year back because it keeps moving for some reason

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
 <?php
 declare(strict_types=1);
 ?>
-<hr><p class="copyright"><i>&copy; <a href="https://nickandfloppy.com/" target="_blank">nick and floppy</a> <?=date('Y')?></i></p>
+<hr><p class="copyright"><i>&copy; <?=date('Y')?> <a href="https://nickandfloppy.com/" target="_blank">nick and floppy</a></i></p>
